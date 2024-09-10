c924e6891f454c8970d3d3166edcfadceb41179f
Timtyger <adebortimothy@gmail.com>
2024-09-10 09:39:48 +0100

    Add project files.

---
+++ LibraryManagementAPI/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+var Configuration = builder.Configuration;
+// Add services to the container.
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = Configuration["Google:client_id"] ??"";
+    googleOptions.ClientSecret = Configuration["Google:client_secret"] ?? "";
+});
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("AppOwner", policy => policy.RequireRole("AppOwner"))
+    .AddPolicy("Customer", policy => policy.RequireRole("Customer"));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
