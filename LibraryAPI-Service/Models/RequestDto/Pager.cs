1a2eed1bec4e15323550041858c103c4e2a508df
Timtyger <adebortimothy@gmail.com>
2024-09-13 20:40:27 +0100

    added requeired attribute to pager for get books

--- LibraryAPI-Service/Models/RequestDto/Pager.cs
+++ LibraryAPI-Service/Models/RequestDto/Pager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace LibraryAPI_Service.Models.RequestDto
 {
     public class Pager
     {
-        [GreaterThanZero]public int PageNumber { get; set; } = 1;
-        [GreaterThanZero] public int PageSize { get; set; } = 10;
+        [GreaterThanZero, Required]public int PageNumber { get; set; } = 1;
+        [GreaterThanZero, Required] public int PageSize { get; set; } = 10;
     }
 }
